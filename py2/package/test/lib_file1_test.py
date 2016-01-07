7049dfdfd5d9f0f7f79bdeff46e2226900a9c861
Shitaibin <james.shi.ch@gmail.com>
2016-01-07 20:37:57 +0800

    learning package and module

--- py2/package/test/lib_file1_test.py
+++ py2/package/test/lib_file1_test.py
@@ -1,24 +1,27 @@
 # Put unittest files and orther files in different directorys.
 # How to make it work.
 
-# 1. make lib directory to be a package by using __init.py
+# 1. make lib directory to be a package by using __init__.py
 # 2. append search directory to sys.path(BUT THIS MAY BE NOT A GOOD WAY)
 
-import sys
-sys.path.append("..")
+########################################
+# See pep328, pep338, pep366
+########################################
 
-import unittest
-
-import lib.lib_file1 as lf
-
-class LibFile1TestCase(unittest.TestCase):
-    """
-    """
-    def test_function1(self):
-        """
-        """
-        lf.function1()
+# import sys
+# sys.path.append("..")
 
 
-if __name__ == '__main__':
-    unittest.main()
+# way 1: import module
+from ..lib import lib_file1 as lf
+lf.function1()
+
+# way 2: from module import function
+from ..lib.lib_file1 import function1
+
+# way 3: from package import function
+# see ../lib/__init__.py, I set all,
+# so I can using function1 without explict
+# import lib_file1
+from ..lib import function1
+function1()
